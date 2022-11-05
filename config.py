de276b71e101e6e091974c5f7ba75c58a062a599
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2022-11-05 19:39:59 +0800

    add sqlite into gitignore

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 from Melodie import Config
 
 config = Config(
-    project_name="Fan",
+    project_name="Demo",
     project_root=os.path.dirname(__file__),
     input_folder="data/input",
     output_folder="data/output",
